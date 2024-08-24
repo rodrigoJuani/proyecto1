7e2353c4132e30a726bbe97558f7a1defde2163d
MrRobot <juaniquinapokar@gmail.com>
2024-08-24 17:52:37 -0400

    poniendo imagen

---
+++ Components/Header.jsx
@@ -0,0 +1,14 @@
+import { assets } from "@/Assets/assets";
+import Image from "next/image";
+import React from "react";
+const Header=()=>{
+    return (
+        <div className="py-5 px-5 md:px-12 lg:px-28">
+            <div className="flex justify-between items-center">
+                <Image src={assets.logo} width={180} alt='' className='w-[130px] sm:w-auto'/>
+
+            </div>
+        </div>
+    )
+}
+export default Header
