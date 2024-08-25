3efc506661e512fe092a47e406355fd1ff2d8d8b
MrRobot <juaniquinapokar@gmail.com>
2024-08-25 15:30:35 -0400

    boton

--- Components/Header.jsx
+++ Components/Header.jsx
@@ -6,7 +6,7 @@ const Header=()=>{
         <div className="py-5 px-5 md:px-12 lg:px-28">
             <div className="flex justify-between items-center">
                 <Image src={assets.logo} width={180} alt='' className='w-[130px] sm:w-auto'/>
-
+                <button className="flex items-center gap-2 font-medium py-1 px-3 sm:py-3 sm:px-6 border-solid border-black"></button>
             </div>
         </div>
     )
