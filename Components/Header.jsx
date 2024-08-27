41360220404b4d85610dce0bd014261ee46f6487
MrRobot <juaniquinapokar@gmail.com>
2024-08-27 07:54:19 -0400

    poniendo recuadro debajo del texto (enter you email)

--- Components/Header.jsx
+++ Components/Header.jsx
@@ -11,6 +11,9 @@ const Header=()=>{
             <div className="text-center my-8">
               <h1 className="text-3x1 sm:text-5xl font-medium">Latest Blogs</h1>
               <p className="mt-10 max-w-[740px] m-auto text-xs sm:text-base">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam voluptate eum veritatis, obcaecati reprehenderit odio, minima totam ex exercitationem officia quidem excepturi aut, nihil illum doloribus porro eveniet facilis iste.</p>
+              <form className="flex justify-between max-w-[500px] scale-75 sm:scale-100 mx-auto mt-10 border border-black" action="">
+                 <input type="email" placeholder="Enter your email" className="pl-4 outline-none"/>
+              </form>
             </div>
         </div>
     )
