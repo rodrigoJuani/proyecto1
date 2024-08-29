3c72165fcb0e958635a0df9039312122c8eb8804
MrRobot <juaniquinapokar@gmail.com>
2024-08-29 13:00:25 -0400

    poniendo imagen

--- Components/BlogItem.jsx
+++ Components/BlogItem.jsx
@@ -1,8 +1,9 @@
+import { blog_data } from "@/Assets/assets";
 import React from "react";
 const BlogItem=()=>{
     return(
-        <div>
-
+        <div className="max-w-[330px] sm:max-w-[300px] bg-white border border-black hover:shadow-[-7px_7px_0px_#000000]">
+          <Image src={blog_data[0].image} alt="" width={400} height={400} className="border-b border-black"/>
         </div>
     )
 }
--- app/page.js
+++ app/page.js
@@ -1,8 +1,11 @@
+'use client'
+import BlogItem from "@/Components/BlogItem";
 import Header from "@/Components/Header";
 export default function Home() {
   return (
     <>
       <Header/>
+      <BlogItem/>
     </>
   );
 }
