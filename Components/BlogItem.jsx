d556c4fe4693e8c8cca1b1efe926c39f4c45f465
MrRobot <juaniquinapokar@gmail.com>
2024-08-30 18:35:52 -0400

    debajo de la imagen el texto

--- Components/BlogItem.jsx
+++ Components/BlogItem.jsx
@@ -6,6 +6,9 @@ const BlogItem=()=>{
         <div className="max-w-[330px] sm:max-w-[300px] bg-white border border-black hover:shadow-[-7px_7px_0px_#000000]">
           <Image src={blog_data[0].image} alt="" width={400} height={400} className="border-b border-black"/>
           <p className="ml-5 mt-5 px-1 inline-block bg-black text-white text-sm">{blog_data[0].category}</p>
+          <div className="p-5">
+            <h5 className="mb-2 text-lg font-medium  tracking-tight text-gray-9000">{blog_data[0].title}</h5>
+          </div>
         </div>
     )
 }
