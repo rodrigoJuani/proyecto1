40913f3b89a8e4be2399e957be9e0c165580d0f4
MrRobot <juaniquinapokar@gmail.com>
2024-08-30 18:29:09 -0400

    recuadro con texto debajo de la imagen

--- Components/BlogItem.jsx
+++ Components/BlogItem.jsx
@@ -5,6 +5,7 @@ const BlogItem=()=>{
     return(
         <div className="max-w-[330px] sm:max-w-[300px] bg-white border border-black hover:shadow-[-7px_7px_0px_#000000]">
           <Image src={blog_data[0].image} alt="" width={400} height={400} className="border-b border-black"/>
+          <p className="ml-5 mt-5 px-1 inline-block bg-black text-white text-sm">{blog_data[0].category}</p>
         </div>
     )
 }
