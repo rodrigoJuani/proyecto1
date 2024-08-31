df851dbdb3c049ab40cdb28215979dcc091f75f4
MrRobot <juaniquinapokar@gmail.com>
2024-08-31 14:21:22 -0400

    texto debajo del titulo_A detailed strep

--- Components/BlogItem.jsx
+++ Components/BlogItem.jsx
@@ -7,7 +7,8 @@ const BlogItem=()=>{
           <Image src={blog_data[0].image} alt="" width={400} height={400} className="border-b border-black"/>
           <p className="ml-5 mt-5 px-1 inline-block bg-black text-white text-sm">{blog_data[0].category}</p>
           <div className="p-5">
-            <h5 className="mb-2 text-lg font-medium  tracking-tight text-gray-9000">{blog_data[0].title}</h5>
+            <h5 className="mb-2 text-lg font-medium  tracking-tight text-gray-900">{blog_data[0].title}</h5>
+            <p className="mb-3 text-sm tracking-tight text-gray-700">{blog_data[0].description}</p>
           </div>
         </div>
     )
