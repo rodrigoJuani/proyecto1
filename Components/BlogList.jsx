a72a1a030bd3f7ea79f59872bd499337c9fad78f
MrRobot <juaniquinapokar@gmail.com>
2024-09-02 12:24:59 -0400

    botones debajo de suscribir

--- Components/BlogList.jsx
+++ Components/BlogList.jsx
@@ -2,7 +2,12 @@ import React from "react";
 const BlogList=()=>{
     return(
        <div>
-        
+        <div className="flex justify-center gap-6 my-10">
+            <button className="bg-black text-white py-1 px4 rounded-sm">ALL</button>
+            <button>Technology</button>
+            <button>Startup</button>
+            <button>lifestyle</button>
+        </div>
        </div>
     )
 }
--- app/page.js
+++ app/page.js
@@ -1,11 +1,11 @@
 'use client'
-import BlogItem from "@/Components/BlogItem";
+import BlogList from "@/Components/BlogList";
 import Header from "@/Components/Header";
 export default function Home() {
   return (
     <>
       <Header/>
-      <BlogItem/>
+      <BlogList/>
     </>
   );
 }
