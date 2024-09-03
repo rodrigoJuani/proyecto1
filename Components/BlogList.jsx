6b317e135e2c1e5ad362c8e12765ca83334615fa
MrRobot <juaniquinapokar@gmail.com>
2024-09-03 09:17:22 -0400

    navegacion

--- Components/BlogList.jsx
+++ Components/BlogList.jsx
@@ -2,13 +2,14 @@ import { blog_data } from "@/Assets/assets";
 import React from "react";
 import BlogItem from "./BlogItem";
 const BlogList=()=>{
+    const [menu,setMenu] = useState("ALL");
     return(
        <div>
         <div className="flex justify-center gap-6 my-10">
-            <button className="bg-black text-white py-1 px4 rounded-sm">ALL</button>
-            <button>Technology</button>
-            <button>Startup</button>
-            <button>lifestyle</button>
+            <button onClick={()=>setMenu("ALL")} className={menu==="ALL"? "bg-black text-white py-1 px4 rounded-sm":""}>ALL</button>
+            <button onClick={()=>setMenu("Technology")} className={menu==="Technology"? "bg-black text-white py-1 px4 rounded-sm":""}>Technology</button>
+            <button onClick={()=>setMenu("Startup")} className={menu==="Startup"? "bg-black text-white py-1 px4 rounded-sm":""}>Startup</button>
+            <button onClick={()=>setMenu("Lifestyle")} className={menu==="Lifestyle"? "bg-black text-white py-1 px4 rounded-sm":""}>lifestyle</button>
         </div>
         <div className="flex flex-wrap justify-around gap-1 gap-y-10 mb-16 xl:mx-24">
           {blog_data.map((item,index)=>{
