3055fcc41efd898753079780fa63b6a237eb87e3
MrRobot <juaniquinapokar@gmail.com>
2024-09-05 12:12:22 -0400

    footer_derechos reservados

--- Components/Footer.jsx
+++ Components/Footer.jsx
@@ -5,6 +5,7 @@ const Footer=()=>{
     return(
         <div className="flex justify-around flex-col gap-2 sm:gap-0 sm:flex-row bg-black py-5 items-center">
          <Image src={assets.logo_light} alt="" width={120}/>
+         <p className="text-sm text-white">ALL right reserved. Copyright @blogger</p>
         </div>
     )
 }
