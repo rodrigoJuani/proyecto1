6e1747f9ae753d9ef3d281975297511bef6f1b8c
MrRobot <juaniquinapokar@gmail.com>
2024-09-08 10:27:39 -0400

    en page agregado de cabecera de blogger

--- app/blogs/[id]/page.jsx
+++ app/blogs/[id]/page.jsx
@@ -1,5 +1,6 @@
 'use client'
-import { blog_data } from "@/Assets/assets";
+import { assets, blog_data } from "@/Assets/assets";
+import Image from "next/image";
 import React, { useEffect,useState } from "react";
 
 const page=({params})=>{
@@ -18,8 +19,13 @@ const page=({params})=>{
         fetchBlogData();
     },[])
     return(
-        <div>
-            {params.id}
+        <div className="bg-gray-200 py-5 px-5 md:px-12 lg:px-28">
+            <div className="flex justify-between items-center">
+              <Image src={assets.logo} width={180} alt="" className="w-[130px] sm:w-auto"/>
+              <button className="flex items-center gap-2 font-medium py-1 px-3 sm:py-3 sm:px-6 border border-black shadow-[-7px_7px_0px_#000000]">
+                Get Started <Image src={assets.arrow} alt=""/>
+              </button>
+            </div>
         </div>
     )
 }
