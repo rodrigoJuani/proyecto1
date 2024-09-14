fea5e3deebfba2594a2fa1b326646ed99d7a1aa5
MrRobot <juaniquinapokar@gmail.com>
2024-09-14 13:15:28 -0400

    mongob

--- app/blogs/[id]/page.jsx
+++ app/blogs/[id]/page.jsx
@@ -1,6 +1,8 @@
 'use client'
 import { assets, blog_data } from "@/Assets/assets";
+import Footer from "@/Components/Footer";
 import Image from "next/image";
+import Link from "next/link";
 import React, { useEffect,useState } from "react";
 
 const page=({params})=>{
@@ -21,7 +23,9 @@ const page=({params})=>{
     return(data?<>
         <div className="bg-gray-200 py-5 px-5 md:px-12 lg:px-28">
             <div className="flex justify-between items-center">
+              <Link href='/'>
               <Image src={assets.logo} width={180} alt="" className="w-[130px] sm:w-auto"/>
+              </Link>
               <button className="flex items-center gap-2 font-medium py-1 px-3 sm:py-3 sm:px-6 border border-black shadow-[-7px_7px_0px_#000000]">
                 Get Started <Image src={assets.arrow} alt=""/>
               </button>
@@ -59,6 +63,7 @@ const page=({params})=>{
             </div>
         </div>
         </div>
+        <Footer/>
         </>:<></>
     )
 }
