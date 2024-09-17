54043eea6734f5ebce67e0826c9934223bae800b
MrRobot <juaniquinapokar@gmail.com>
2024-09-17 09:00:50 -0400

    base de datos para la img

--- app/api/blog/route.js
+++ app/api/blog/route.js
@@ -1,5 +1,19 @@
+import { ConnectDB } from "@/lib/config/db"
+
 const{ NextResponse }=require("next/server")
+const  LoadDB=async()=>{
+    await ConnectDB();
+}
+LoadDB();
 export async function GET(request) {
-    console.log("Blog Get Hit");
     return NextResponse.json({msg:"API working"})
+}
+export async function POST(request) {
+    const formData=await request.formData();
+    const timestamp=Date.now();
+
+    const image=FormData.get('image');
+    const imageByteData=await image.arrayBuffer();
+    const buffer=Buffer.from(imageByteData);
+    const path=`./public/${timestamp}`
 }
