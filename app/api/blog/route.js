b8ea16534bca9c2ef3d2cbccc92deee30b1ff186
MrRobot <juaniquinapokar@gmail.com>
2024-09-19 11:56:10 -0400

    continuando formulario para la BD

--- app/api/blog/route.js
+++ app/api/blog/route.js
@@ -1,6 +1,6 @@
 import { ConnectDB } from "@/lib/config/db"
-
-const{ NextResponse }=require("next/server")
+const{ NextResponse }=require("next/server");
+import{writeFile} from 'fs/promises'
 const  LoadDB=async()=>{
     await ConnectDB();
 }
@@ -12,8 +12,12 @@ export async function POST(request) {
     const formData=await request.formData();
     const timestamp=Date.now();
 
-    const image=FormData.get('image');
+    const image=formData.get('image');
     const imageByteData=await image.arrayBuffer();
     const buffer=Buffer.from(imageByteData);
-    const path=`./public/${timestamp}`
+    const path=`./public/${timestamp}_${image.name}`;
+    await writeFile(path,buffer);
+    const imgUrl=`/${timestamp}_${image.name}`;
+    console.log(imgUrl);
+    return NextResponse.json({imgUrl})
 }
