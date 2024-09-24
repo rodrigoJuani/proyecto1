31029fc6a78684a1c4349154b2cf261275499e5c
MrRobot <juaniquinapokar@gmail.com>
2024-09-24 07:47:58 -0400

    Agregando nuevas carpetas

---
+++ Components/AdminComponents/Sidebar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+const Sidebar =()=>{
+    return(
+        <div className="flex flex-col bg-slate-100">
+            <div className="px-2 sm:pl-14 py-3 border border-black">
+                
+            </div>
+        </div>
+    )
+}
+export default Sidebar
