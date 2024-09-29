88821412a2af1920162fd68e59c084dd0d50563b
MrRobot <juaniquinapokar@gmail.com>
2024-09-28 22:39:22 -0400

    agregando boton para enviar el blog

--- app/admin/addProduct/page.jsx
+++ app/admin/addProduct/page.jsx
@@ -4,6 +4,13 @@ import Image from "next/image";
 import { assets } from "@/Assets/assets";
 const page=()=>{
     const [image,setImage]=useState(false);
+    const [data,setData]=useState({
+        title:"",
+        description:"",
+        category:"Startup",
+        author:"Alex Bennet",
+        authorImg:""
+    })
     return(<div>
         <>
         <form className="pt-5 px-5 sm:pt-12 sm:pl-16">
@@ -22,6 +29,8 @@ const page=()=>{
                 <option value="Technology">Technology</option>
                 <option value="Lifestyle">Lifestyle</option>
             </select>
+            <br/>
+            <button type="submit" className="mt-8 w-40 h-12 bg-black text-white">ADD</button>
         </form>
         </>
     </div>
