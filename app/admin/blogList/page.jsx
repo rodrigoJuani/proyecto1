f8bd9982ac95ff6b809281a05c4f56b98e67ab65
MrRobot <juaniquinapokar@gmail.com>
2024-10-06 12:55:22 -0400

    correcion1

--- app/admin/blogList/page.jsx
+++ app/admin/blogList/page.jsx
@@ -2,8 +2,10 @@
 import React from "react";
 
 const page=()=>{
-    return(<div>
-
+    return(<div className="flex-1 pt-5 px-5 sm:pt-12 sm:pl-16">
+        <h1>ALL blogs</h1>
+        <div className="relative h-[80vh] max-w-[850px] overflow-x-auto mt-4 border border-gray-400 scrollbar-hide">
+        </div>
     </div>
     )
 }
