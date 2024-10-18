db02ff181c58b32ae865b12aa8fc0ccf32e15a9e
MrRobot <juaniquinapokar@gmail.com>
2024-10-17 22:43:35 -0400

    correccion0

--- Components/Header.jsx
+++ Components/Header.jsx
@@ -1,13 +1,25 @@
 import { assets } from "@/Assets/assets";
+import axios from "axios";
 import Image from "next/image";
 import React,{useState} from "react";
+import { toast } from "react-toastify";
+
 const Header=()=>{
+
     const [email,setEmail]=useState("");
     
     const onSubmitHandler=async(e)=>{
         e.preventDefault();
         const formData=new FormData();
-        formData.append("email",email)
+        formData.append("email",email);
+        const response=await axios.post('../app/api/email',formData);
+        if(response.data.success){
+            toast.success(response.data.msg);
+            setEmail("");
+        }
+        else{
+            toast.error("Error");
+        }
     }
     
     return (
@@ -20,7 +32,7 @@ const Header=()=>{
               <h1 className="text-3xl sm:text-5xl font-medium">Latest Blogs</h1>
               <p className="mt-10 max-w-[740px] m-auto text-xs sm:text-base">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam voluptate eum veritatis, obcaecati reprehenderit odio, minima totam ex exercitationem officia quidem excepturi aut, nihil illum doloribus porro eveniet facilis iste.</p>
               <form onSubmit={onSubmitHandler} className="flex justify-between max-w-[500px] scale-75 sm:scale-100 mx-auto mt-10 border border-black shadow-[-7px_7px_0px_#000000]" action="">
-                 <input onChange={(e)=>setEmail(e.target.value)} type="email" placeholder='Enter your email' className="pl-4 outline-none"/>
+                 <input onChange={(e)=>setEmail(e.target.value)} value={email} type="email" placeholder='Enter your email' className="pl-4 outline-none"/>
                  <button type="submit" className="border-1 border-black py-4 px-4 sm:px-8 active:bg-gray-600 active:text-white">
                     Subscribe
                  </button>
--- app/api/email/route.js
+++ app/api/email/route.js
@@ -4,14 +4,15 @@ import { NextResponse } from "next/server";
 
 const LoadDB=async()=>{
     await ConnectDB();
-}
+};
 LoadDB();
 
 export async function POST(request) {
     const formData=await request.formData();
-    const email={
+    const emailData={
         email:`${formData.get('email')}`,
     }
     await EmailModel.create(emailData);
     return NextResponse.json({success:true,msg:"Email Subscribed"})
 }
+
