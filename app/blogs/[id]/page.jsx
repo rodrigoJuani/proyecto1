96342f3e1c862d49bc96adf11ebcba6b997882be
MrRobot <juaniquinapokar@gmail.com>
2024-10-22 20:57:34 -0400

    FIN

--- app/blogs/[id]/page.jsx
+++ app/blogs/[id]/page.jsx
@@ -40,22 +40,9 @@ const page=({params})=>{
         </div>
         <div className="mx-5 max-w-[800px] md:mx-auto mt-[-100px] mb-10">
          <Image className="border-4 border-white" src={data.image} width={1280} height={720} alt=""/>
-         <h1 className="my-8 text-[26px] font-semibold">Introduction:</h1>
-         <p>{data.description}</p>
-         <h3 className="my-5 text-[18px] font-semibold">Step 1: Self-Reflection and Goal Setting</h3>
-         <p className="my-3 ">Before you can manage your lifestyle, you must have a clear understanding of what you want to archieve.Strart by reflecting on your values,aspirations, and long-term goals</p>
-         <p className="my-3 ">Before you can manage your lifestyle, you must have a clear understanding of what you want to archieve.Strart by reflecting on your values,aspirations, and long-term goals</p>
 
-         <h3 className="my-5 text-[18px] font-semibold">Step 2: Self-Reflection and Goal Setting</h3>
-         <p className="my-3 ">Before you can manage your lifestyle, you must have a clear understanding of what you want to archieve.Strart by reflecting on your values,aspirations, and long-term goals</p>
-         <p className="my-3 ">Before you can manage your lifestyle, you must have a clear understanding of what you want to archieve.Strart by reflecting on your values,aspirations, and long-term goals</p>
-
-         <h3 className="my-5 text-[18px] font-semibold">Step 3: Self-Reflection and Goal Setting</h3>
-         <p className="my-3 ">Before you can manage your lifestyle, you must have a clear understanding of what you want to archieve.Strart by reflecting on your values,aspirations, and long-term goals</p>
-         <p className="my-3 ">Before you can manage your lifestyle, you must have a clear understanding of what you want to archieve.Strart by reflecting on your values,aspirations, and long-term goals</p>
-        
-         <h3 className="my-5 text-[18px] font-semibold">Conclusion:</h3>
-         <p className="my-3 ">Manage your lifestylr is a journey that requires commitment and self-awareness.By Following this step-by-</p>
+         <div className="blog-content" dangerouslySetInnerHTML={{__html:data.description}}></div>
+         
         <div className="my-24">
             <p className="text-black font font-semibold my-4">Share this article on social media</p>
             <div className="flex">
